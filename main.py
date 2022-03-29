83143f71093601627273da1b5952a82c90580b5f
konsash <konnovsasha2003@inbox.ru>
2022-03-29 07:44:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import speech_recognition as sr
 from fuzzywuzzy import fuzz
 import pyttsx3
 import datetime
+import subprocess
+from selenium import webdriver
+
 
 opts = {
     "name": ("маруся", "марусь"),
-    "tbr": ("скажи", "расскажи", "покажи", "сколько", "произнеси"),
+    "tbr": ("скажи", "расскажи", "покажи", "сколько", "произнеси", "открой", "хочу сделать", "создай", "давай посмотрим"),
     "cmds": {
         "ctime": ("текущее время", "сейчас времени", "который час"),
-        "stupid1": ('расскажи анекдот', 'рассмеши меня', 'ты знаешь анекдоты')
+        "stupid1": ('расскажи анекдот', 'рассмеши меня', 'ты знаешь анекдоты'),
+        "fast_access_folder": ('папку'),
+        "fast_access_notepad": ('блокнот'),
+        "fast_access_word": ('документ'),
+        "chrome_youtube" : ('видео')
     }
 }
 
@@ -51,8 +58,17 @@ def execute_cmd(cmd):
     if cmd == 'ctime':
         now = datetime.datetime.now()
         speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
-    elif cmd == "stupid1":
+    elif cmd == 'stupid1':
         speak("Мой разработчик не научил меня анекдотам ... Ха ха ха")
+    elif cmd == 'fast_access_folder':
+        subprocess.Popen('explorer "%s"' % ("abc"), cwd="C:/Users/Александр/Desktop/getter")
+    elif cmd == 'fast_access_notepad':
+        subprocess.Popen('C:/Windows/system32/notepad.exe')
+    elif cmd == 'fast_access_word':
+        subprocess.Popen('C:/Program Files (x86)/Microsoft Office/Office16/WINWORD.EXE')
+    elif cmd == 'chrome_youtube':
+        browser = webdriver.Chrome()
+        browser.get('https://www.youtube.com/')
     else:
         speak("Команда не распознана повторите")
 
